433751c64437f6a68ccebc53477413bb5db2d438
JaySmith <jay@jaysmith.us>
2015-08-25 09:22:22 -0500

    initial

---
+++ MarbleTasks/MarbleTasks/FormMain.Designer.cs
@@ -0,0 +1,198 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: smithjay
+ * Date: 8/25/2015
+ * Time: 8:39 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace MarbleTasks
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton toolStripButtonNewTask;
+		private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem synchronizeActiveAcccountToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem synchromizeAllAccountsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem reinitializeActiveAccountToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem onlineHelpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButtonNewTask = new System.Windows.Forms.ToolStripButton();
+			this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.synchronizeActiveAcccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.synchromizeAllAccountsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.reinitializeActiveAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+			this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.onlineHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.fileToolStripMenuItem,
+			this.toolsToolStripMenuItem,
+			this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(667, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripButtonNewTask});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(667, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripButtonNewTask
+			// 
+			this.toolStripButtonNewTask.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButtonNewTask.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonNewTask.Image")));
+			this.toolStripButtonNewTask.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButtonNewTask.Name = "toolStripButtonNewTask";
+			this.toolStripButtonNewTask.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButtonNewTask.Text = "New Task";
+			// 
+			// toolsToolStripMenuItem
+			// 
+			this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.synchronizeActiveAcccountToolStripMenuItem,
+			this.synchromizeAllAccountsToolStripMenuItem,
+			this.reinitializeActiveAccountToolStripMenuItem,
+			this.toolStripMenuItem1,
+			this.optionsToolStripMenuItem});
+			this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+			this.toolsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.toolsToolStripMenuItem.Text = "Tools";
+			// 
+			// synchronizeActiveAcccountToolStripMenuItem
+			// 
+			this.synchronizeActiveAcccountToolStripMenuItem.Name = "synchronizeActiveAcccountToolStripMenuItem";
+			this.synchronizeActiveAcccountToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.synchronizeActiveAcccountToolStripMenuItem.Text = "Synchronize Active Acccount";
+			// 
+			// synchromizeAllAccountsToolStripMenuItem
+			// 
+			this.synchromizeAllAccountsToolStripMenuItem.Name = "synchromizeAllAccountsToolStripMenuItem";
+			this.synchromizeAllAccountsToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.synchromizeAllAccountsToolStripMenuItem.Text = "Synchromize All Accounts";
+			// 
+			// reinitializeActiveAccountToolStripMenuItem
+			// 
+			this.reinitializeActiveAccountToolStripMenuItem.Name = "reinitializeActiveAccountToolStripMenuItem";
+			this.reinitializeActiveAccountToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.reinitializeActiveAccountToolStripMenuItem.Text = "Reinitialize Active Account";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(225, 6);
+			// 
+			// optionsToolStripMenuItem
+			// 
+			this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+			this.optionsToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.optionsToolStripMenuItem.Text = "Options...";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Text = "E&xit";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.onlineHelpToolStripMenuItem,
+			this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "Help";
+			// 
+			// onlineHelpToolStripMenuItem
+			// 
+			this.onlineHelpToolStripMenuItem.Name = "onlineHelpToolStripMenuItem";
+			this.onlineHelpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.onlineHelpToolStripMenuItem.Text = "Online Help...";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.aboutToolStripMenuItem.Text = "&About";
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(667, 480);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "FormMain";
+			this.Text = "Marble Tasks - A Google Task Client";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MarbleTasks/MarbleTasks/FormMain.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: smithjay
+ * Date: 8/25/2015
+ * Time: 8:39 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MarbleTasks
+{
+	/// <summary>
+	/// Description of FormMain.
+	/// </summary>
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
